82aa375f9bb22ba6b1fec23fd48c36112fd7960b
Ryan.Syder <rsmostlyharmless@gmail.com>
2022-03-03 19:11:28 +0100

    changes made to exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(rts) {
+    let gnirts = "";
+    for (let i = rts.length - 1; i >= 0; i--) {
+        gnirts += rts[i];
+    }
+    return gnirts;
 };
 
 // Do not edit below this line
