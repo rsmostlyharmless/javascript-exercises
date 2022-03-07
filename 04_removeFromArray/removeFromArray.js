8bbfcfaf67185354e8159707e7b21bca489a9498
Ryan.Syder <rsmostlyharmless@gmail.com>
2022-03-07 13:08:57 +0100

    changes made to exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,21 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...remove) {
+    let array = remove[0];
+    let newArray = [];
+    array.forEach((item) => {
+        if (!remove.includes(item)) {
+            newArray.push(item);
+        }
+    });
+    return newArray;
 };
+
+//Learn more about ! and forEach.
+
+//Keep this for future learning.
+// var removeFromArray = function(...args) {
+//   const array = args[0]
+//   return array.filter(val => !args.includes(val))
+// }
 
 // Do not edit below this line
 module.exports = removeFromArray;
