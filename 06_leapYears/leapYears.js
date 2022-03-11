97c2d4e138789aa4d68d66fe4e285e4c7d4c300f
Ryan.Syder <rsmostlyharmless@gmail.com>
2022-03-11 10:12:38 +0100

    changes made to exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(leap) {
+    if ((0 == leap % 4) && (0 != leap % 100) || (0 == leap % 400)) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
