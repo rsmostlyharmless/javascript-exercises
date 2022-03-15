987ff2e240b50ca948b643c7670b44ae0fb43c52
Ryan.Syder <rsmostlyharmless@gmail.com>
2022-03-15 11:15:26 +0100

    changes made to exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+    let num1 = 1;
+    let num2 = 0;
+    let sum;
+
+    if (num < 0) {
+        return 'OOPS';
+    } 
+    else
+
+    for (let i = 0; i < num; i++) {
+        sum = num1 + num2;
+        num1 = num2;
+        num2 = sum;
+    }
+    return num2;
 };
 
 // Do not edit below this line
