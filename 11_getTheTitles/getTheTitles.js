ef85799517ff2fe2ad92fff104d362f7e7c7eea2
Ryan.Syder <rsmostlyharmless@gmail.com>
2022-03-15 15:53:13 +0100

    changes made to exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,21 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(value) {
+    return value.map(book => book.title);
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
+
+/*
+function updateRecords(records, id, prop, value) {
+      if (value === '') {
+        delete records[id][prop];
+      } else if (prop === 'tracks') {
+        records[id][prop] = records[id][prop] || [];
+        records[id][prop].push(value);
+      } else {
+        records[id][prop] = value;
+      }
+      return records;
+    }
+*/
